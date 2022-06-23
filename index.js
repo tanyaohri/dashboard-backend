aac5e9242abea8fb1e8d9a0a2cffaa9233eab42c
Tanya Ohri <tanyaohri08@gmail.com>
2022-06-23 10:11:03 +0530

    initial poc

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 require("dotenv").config();
 
-const express = require('express'),
-        app   = express();
+const express       = require('express'),
+        jwt         = require("jsonwebtoken"),
+        app         = express();
+
+app.use(express.json());
 
 const port  = process.env.PORT || 8000
+const jwt_secret_token = process.env.JWT_SECRET_ACCESS_TOKEN
+
+const posts = [
+    {
+        username:"piyush",
+        post:"post1"
+    },
+    {
+        username:"tanya",
+        post:"post2"
+    }
+]
+
+app.get("/posts", (req, res) =>{
+    res.json(posts)
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    console.log(req.body)
+    if(username===undefined || username===null){
+        res.send({
+            message: "Please provide username"
+        });
+        return
+    }
+    const user = { name : username }
+    const access_token = jwt.sign(user, jwt_secret_token);
+    res.send({
+        message:`Hey there ${username}`,
+        accessToken : access_token
+    })
+})
+
+app.post("/", (req, res) => {
+    console.log("hi")
+    console.log(req.body)
+    res.send(req.body);
+})
+
 
 
 app.listen(port, (err) => {
